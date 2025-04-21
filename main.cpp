5a8abd04131bf7d8a7e04907b70fb0c104a77409
WakamatuSanga <k024g0094@g.neec.ac.jp>
2025-04-21 09:41:45 +0900

    00_04の続けき

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include<Windows.h>
 #include<cstdint>
+#include<string>
+#include<format>
+#include<filesystem>// ファイルやディレクトリに関する操作を行うライブラリ
+#include<fstream>	// ファイルに描いたり読んだりするライブラリ
+#include<chrono>	//時間を扱うライブラリ
 //ウィンドウプロシージャ
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 	WPARAM wparam, LPARAM lparam) {
@@ -14,6 +19,38 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg,
 	//標準もメッセージ処理を行う
 	return DefWindowProc(hwnd, msg, wparam, lparam);
 }
+
+void Log(/*std::ofstream& os,*/ const std::string& message) {
+	//os << message << std::endl;
+	OutputDebugStringA(message.c_str());
+}
+
+
+
+//std::wstring ConvertString(const std::string& str) {
+//	if (str.empty())
+//	{
+//		return std::wstring();
+//	}
+//}
+
+
+std:: string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0,
+	str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+if (sizeNeeded == 0) {
+	return std::string();
+}
+std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>
+		(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
@@ -53,13 +90,35 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		nullptr,			//メニューハンドル
 		wc.hInstance,		//インスタンスハンドル
 		nullptr);		//オプション
+	
 
 	//ウインドウを表示する
 	ShowWindow(hwnd, SW_SHOW);
 
-	//出力ウインドウへの文字出力
-	OutputDebugStringA("Hello,DirectX!\n");
+	// ログのディレクトリを用意
+	std::filesystem::create_directory("logs");
+	//現在時刻を取得(UTC時刻)
+	std::chrono::system_clock::time_point now = std::chrono::system_clock::now();
+	//ログファイルの名前にコンマ何秒はいらないので、削って秒にする
+	std::chrono::time_point<std::chrono::system_clock, std::chrono::seconds>
+	nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
+	//日本時間(PCの設定時間)に変換
+	std::chrono::zoned_time localTime{ std::chrono::current_zone(),nowSeconds };
+	//formatを使って年月日_時分秒の文字列に変換
+	std::string dateString = std::format("{:%Y%m%d_%H%M%S}", localTime);
+	//時間を使ってファイル名を決定
+	std::string logFilePath = std::string("logs/") + dateString + ".log";
+	//ファイルを使って書き込み準備
+	std::ofstream logStream(logFilePath);
 
+	//文字列を格納する
+	std::string str0{ "STRING!!!" };
+
+	//整数を文字列にする
+	std::string str1{ std::to_string(10) };
+	Log(ConvertString(std::format(L"WSTRING{}\n", L"abc")));
+	
+	
 	MSG msg{};
 	//ウインドウの×ボタンが押されるまでループ
 	while (msg.message != WM_QUIT){
