20ddb39c437c48f773617e1b8fff052f419fd7bd
WakamatuSanga <k024g0094@g.neec.ac.jp>
2025-10-03 10:58:31 +0900

    00_01の途中（imguiカラーなし）

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -1,23 +1,57 @@
-#include"Object3d.hlsli"
+#include "Object3d.hlsli"
 
 struct Material
 {
     float32_t4 color;
+    int32_t enableLighting;
+    float32_t4x4 uvTransform;
 };
+
+struct DirecctionalLight
+{
+    float32_t4 color; //!<ライトの色
+    float32_t3 direction; //!< ライトの向き
+    float intensity; //!<輝度
+};
+
 Texture2D<float32_t4> gTexture : register(t0);
 SamplerState gSampler : register(s0);
 ConstantBuffer<Material> gMaterial : register(b0);
+ConstantBuffer<DirecctionalLight> gDirectionalLight : register(b1);
 
 struct PixelShaderOutput
 {
     float32_t4 color : SV_Target0;
 };
 
+
+
+
 PixelShaderOutput main(VertexShaderOutput input)
 {
     PixelShaderOutput output;
     output.color = gMaterial.color;
-    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
-    output.color = gMaterial.color * textureColor;
+
+     // UV変換
+    float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f), gMaterial.uvTransform);
+
+    float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
+
+    if (gMaterial.enableLighting != 0)//Lightingする場合
+    {
+
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
+        output.color.rgb = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        output.color.a = gMaterial.color.a * textureColor.a;
+    }
+    else
+    {
+
+        output.color = gMaterial.color * textureColor;
+
+    }
+
     return output;
+
 }
